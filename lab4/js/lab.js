1e131503180cfcc6ec86297aac2f2b08d5e5bd25
heiikd <146970598+heiikd@users.noreply.github.com>
2023-10-22 22:06:46 -0700

    Lab 4

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,21 +1,14 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js 
+// Rock-Paper-Scissors
+// An implementation of the game
+// Heidi Dullack <hdullack@ucsc.edu>
+// October 2023
 
-// Constants
-
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+// Choose computer move
+  // Choose Rock, Paper, or Scissors
+// Accept user input
+// Validate user move
+// Count 3
+// Count 2
+// Count 1
+// Test for win or draw condition
