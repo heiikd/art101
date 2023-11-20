c9a190a037278f99772c106e3aad93004b5200d7
heiikd <146970598+heiikd@users.noreply.github.com>
2023-11-19 16:00:37 -0800

    lab10

--- lab10/js/lab.js
+++ lab10/js/lab.js
@@ -11,17 +11,16 @@ Code belongs to Wes Modes
 function generateRandomText() {
     const text = "When you were here before Couldn't look you in the eye You're just like an angel Your skin makes me cry  You float like a feather  In a beautiful world   I wish I was special  You're so fuckin' special But I'm a creep I'm a weirdo What the hell am I doin' here? I don't belong here I don't care if it hurts I wanna have control I want a perfect body I want a perfect soul I want you to notice When I'm not around So fuckin' special I wish I was special But I'm a creep I'm a weirdo What the hell am I doin' here? I don't belong here She's running out the door (run) She's running out She run, run, run, run Run Whatever makes you happy Whatever you want You're so fuckin' special I wish I was special But I'm a creep I'm a weirdo What the hell am I doin' here? I don't belong here I don't belong here";
     const min = 4;
-    const max = 150;
+    const max = 100;
     const randLen = Math.floor(Math.random() * (max - min +1)) + min;
     // get a random starting index to slice the radiohead text
     const randStart = Math.floor(Math.random() * (text.length - randLen + 1));
     // generate the random text
-    return text.slice(randStart + randLen);
+    return text.slice(randStart, randStart + randLen);
 }
 
 //add a click listener for the button
 $("#button").on("click", function(){
-});
 
 // get new fake dialogue
 const newText = generateRandomText();
@@ -29,3 +28,4 @@ const newText = generateRandomText();
 //append a div to our new output div
 $("#output").append('<div class="text"><p>' + newText + '</p></div>')
 
+});
